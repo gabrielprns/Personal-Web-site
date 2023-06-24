39e8963cc78e1996c89dead07a8a9116a990a155
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2023-06-23 22:01:15 -0300

    Portfolio

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,30 @@
+import "./banner.css";
+export const Banner = () => {
+  
+    return (
+      
+      <header className="banner">
+        <h1 className="title logo">GP</h1>
+        <div className="container">
+          <a href="" className="title">
+            <h1 >About</h1>
+          </a>
+          <a href="" className="title">
+            <h1 >Portfolio</h1>
+          </a>
+          <a href="https://www.linkedin.com/in/gabriel-pedro-8b08ab1a2/" className="title">
+            <h1 >Linkedin</h1>
+          </a>
+          <a href="https://github.com/gabrielprns" className="title">
+            <h1 >Github</h1>
+          </a>
+          
+          
+        </div>
+        
+      </header>
+    )
+  
+  
+}
+
---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+export const CampoTexto = () =>{
+  return(
+    <div>
+      
+    </div>
+  
+  
+  )
+}
