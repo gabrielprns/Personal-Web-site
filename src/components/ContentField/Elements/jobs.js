d31b17efaa437c29877b930f74deba34b1292a3c
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2023-07-03 20:04:27 -0300

    deploy

--- src/components/ContentField/Elements/jobs.js
+++ src/components/ContentField/Elements/jobs.js
@@ -3,9 +3,7 @@ export const Jobs = () =>{
     
     <div className="title-content  hs">
       <h2 className="title-1" >Projetos</h2>
-      <h1 className="square-box-tec"> 
-         
-      </h1> 
+       
     </div>
   )
   }
